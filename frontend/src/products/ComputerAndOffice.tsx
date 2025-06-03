0a042734d360dc68d7e9e5c23eaff7e7b1b1f2c6
Arif Hossain <hossaininfo@ymail.com>
2025-06-03 11:01:19 +0200

    changed text on computer and office

--- frontend/src/products/ComputerAndOffice.tsx
+++ frontend/src/products/ComputerAndOffice.tsx
@@ -27,7 +27,7 @@ const ComputerAndOffice = () => {
             <section className="px-6 py-12 max-w-7xl mx-auto">
                 <h2 className="text-3xl font-bold mb-4">DATORER OCH KONTOR</h2>
                 <p className="mb-6">
-                    Hos Elgiganten finner du ett omfattande sortiment av datorer och datatillbehör. Bland annat
+                    Hos Electric finner du ett omfattande sortiment av datorer och datatillbehör. Bland annat
                     datorkomponenter, skrivare, hårddiskar och programvaror.
                 </p>
             </section>
