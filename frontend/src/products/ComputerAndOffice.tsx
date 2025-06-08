8b2b6d0f05c5ad32d5c19b772b230b1d0a6e4213
Arif Hossain <hossaininfo@ymail.com>
2025-06-08 13:13:13 +0200

    adde h1 tag on ComputerAccesories

--- frontend/src/products/ComputerAndOffice.tsx
+++ frontend/src/products/ComputerAndOffice.tsx
@@ -1,5 +1,7 @@
 import Footer from "../components/Footer.tsx";
 import {Link} from "react-router-dom";
+import ProductCard from "../banner/ProductCard.tsx";
+import {useAuth} from "../components/AuthContext.tsx";
 
 const menuItems = [
     {label: "Datorer", to: "/computer", image: "/computer-acc.png",},
@@ -18,6 +20,7 @@ const menuItems = [
 ];
 
 const ComputerAndOffice = () => {
+    const {products} = useAuth();
     return (
         <section>
             <header className="p-5">
@@ -48,9 +51,30 @@ const ComputerAndOffice = () => {
                     </Link>
                 ))}
             </section>
-
+            <section className="p-6">
+                <h1 className="p-5 font-bold text-2xl">NÅGOT FÖR DIG?</h1>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+                    {products
+                        .filter((product) => product.category === "LAPTOP")
+                        .map((product) => (
+                            <ProductCard
+                                key={product.id}
+                                image={"/computer.png"}
+                                title={product.title}
+                                reviews={product.reviews ?? 0}
+                                description={product.description}
+                                availability={product.availability ?? "Tillgänglighet okänd"}
+                                price={product.price}
+                                currency={product.currency}
+                                category={product.category}
+                                stock={product.stock}
+                                to={`/product/${product.id}`}
+                            />
+                        ))}
+                </div>
+            </section>
             <footer>
-                <Footer />
+                <Footer/>
             </footer>
         </section>
     );
